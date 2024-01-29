69acde651444b4d210acd8f4f1f12b8cba2f028d
WallFerreira <mr.wallaceferreira@gmail.com>
2024-01-29 18:27:53 -0300

    Iniciado projeto MyShop, utilizando React, typescript e stuled components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+font-size: 2rem;
+color: blue;
+`
+
+
+function App() {
+  return (
+    <Title>Styled components</Title>
+  );
+}
+
+export default App;
